f559313080d096e2f98ba897601c69262bb641d3
Iosvany Alvarez <94980291+ialvarezDEV@users.noreply.github.com>
2022-01-10 23:19:53 -0500

    First Commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <HomeLayout>
+        <MainComponent/>
+    </HomeLayout>
+</template>
+
+
+<script>
+    import HomeLayout from "@/Layouts/HomeLayout";
+    import MainComponent from "@/Components/MainComponent";
+    export default {
+        components: {MainComponent, HomeLayout},
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+        }
+    }
+</script>
